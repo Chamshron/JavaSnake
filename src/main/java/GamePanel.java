e0517157b5b59376326d20d0da2d3cad429e8b70
Chamsron <emmacameron219@gmail.com>
2024-01-12 22:11:10 +0000

    Score is now displayed on game over screen

--- src/main/java/GamePanel.java
+++ src/main/java/GamePanel.java
@@ -140,10 +140,17 @@ public class GamePanel extends JPanel implements ActionListener {
     }
 
     public void gameOver(Graphics g){
+
+        g.setColor(Color.RED );
+        g.setFont(new Font("Ink Free", Font.BOLD, 40));
+        FontMetrics metrics1 = getFontMetrics(g.getFont());
+        g.drawString("Score: "+applesEaten,( SCREEN_WIDTH-metrics1.stringWidth("Score: "+applesEaten))/2, g.getFont().getSize());
+
+
         g.setColor(Color.BLACK);
         g.setFont(new Font("Ink Free", Font.BOLD, 75));
-        FontMetrics metrics = getFontMetrics(g.getFont());
-        g.drawString("Game Over",( SCREEN_WIDTH-metrics.stringWidth(" Game Over "))/2, (SCREEN_HEIGHT/2));
+        FontMetrics metrics2 = getFontMetrics(g.getFont());
+        g.drawString("Game Over",( SCREEN_WIDTH-metrics2.stringWidth(" Game Over "))/2, (SCREEN_HEIGHT/2));
     }
 
     @Override
